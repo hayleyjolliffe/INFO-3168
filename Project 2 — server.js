57478a03c88dc418ccece3cd65a8c9edd539d62f
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-04-12 11:04:04 -0400

    Setting clearing function, finding $gel function

--- Project 2 — server.js
+++ Project 2 — server.js
@@ -6,3 +6,22 @@ let addBox = $gel("address");
 let cityBox = $gel("city");
 let proBox = $gel("province");
 let postBox = $gel("postal");
+
+// NEW button clears all text boxes
+function clearFields() {
+    custBox.innerHTML = "";
+    firstBox.innerHTML = "";
+    lastBox.innerHTML = "";
+    addBox.innerHTML = "";
+    cityBox.innerHTML = "";
+    proBox.innerHTML = "";
+    postBox.innerHTML = "";
+};
+
+//
+
+//
+
+//
+
+//
