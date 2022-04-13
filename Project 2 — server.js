3d0f98df4c40484680a9325788a4271fe7cd32af
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-04-13 11:44:43 -0400

    Set POST handler

--- Project 2 — server.js
+++ Project 2 — server.js
@@ -19,3 +19,32 @@ let express = require('express');
 let parse = require('body-parser');
 let app = express();
 app.use(parse.urlencoded({ extended: false }));
+
+// POST handler
+app.post('/api/data', (request, response) => {
+    let pbody = request.body;
+
+    if (pbody.customer === NOTNULL) {
+        pbody.firstname = $gel(firstname).value;
+        $mesh(pbody.firstname);
+        pbody.lastname = $gel(lastname).value;
+        $mesh(pbody.lastname);
+        pbody.address = $gel(address).value;
+        $mesh(pbody.address);
+        pbody.city = $gel(city).value;
+        $mesh(pbody.city);
+        pbody.province = $gel(province).value;
+        $mesh(pbody.province);
+        pbody.postal = $gel(postal).value;
+        $mesh(pbody.postal);
+        response.send(pbody);
+        console.log(pbody);
+    }
+    else {
+        pbody.first = "Not found";
+        pbody.last = "";
+        pbody.address = "";
+        response.send(pbody);
+        console.log(pbody);
+    }
+});
