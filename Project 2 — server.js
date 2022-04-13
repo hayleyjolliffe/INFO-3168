9db4a8275deee3f63f4039d69cc0bb64b8ceb58b
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-04-13 11:37:44 -0400

    Set port and express

--- Project 2 — server.js
+++ Project 2 — server.js
@@ -13,3 +13,9 @@ let port = process.env.PORT || 1337;
 let server = app.listen(port, function () {
     console.log("Server is running on port " + port);
 });
+
+// Setting up express
+let express = require('express');
+let parse = require('body-parser');
+let app = express();
+app.use(parse.urlencoded({ extended: false }));
