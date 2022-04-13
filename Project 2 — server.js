a855e658a280165aebc6cc9726b64bc4fa22f317
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-04-13 10:18:37 -0400

    Adding Node elements?

--- Project 2 — server.js
+++ Project 2 — server.js
@@ -1,3 +1,18 @@
+// Setting port
+let port = process.env.PORT || 1337;
+let server = app.listen(port, function () {
+    console.log("Server is running on port " + port);
+});
+
+// Setting objects and constructors
+let express = require('express');
+let parser = require('body-parser');
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (request, response) => response.sendFile(__dirname + "/index.html"));
+
 // $gel function for reduced typing
 function $gel(id) {
     return document.getElementById(id);
